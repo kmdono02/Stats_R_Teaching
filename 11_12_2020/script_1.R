3b1fb47b473a4c4c1ae823d062ed0ea4ab2074d1
Kevin Donovan <43554362+kmdono02@users.noreply.github.com>
2020-11-10 15:14:55 -0500

    Add 11/12

---
+++ 11_12_2020/script_1.R
@@ -0,0 +1,103 @@
+### File paths
+## Working directory (WD)
+# Wait what is this?
+getwd() # prints out current working directory. Set to directory where script is
+setwd("blah") # sets working directory to directory named blah.
+# Specify directory of interest as a folder path inside "" as argument
+# WD is the default folder R looks to save and load things.  
+# Also used as starting place to navigate relative path names, see above example
+
+## Absolute path names
+# Suppose I want to change my WD to specific folder.
+# One way: provide whole folder location to R
+setwd("C:\Users\kdono\OneDrive - University of North Carolina at Chapel Hill\Documents\School\Research\Truong Research\Stats_R_Teaching\11_12_2020")
+# Create error.  Why?
+# Annoyingly, Windows and R using different definitions for separators in a file path name.
+# R and Apple use / BUT Windows uses \ which means something different in R.  Thus we need ...
+setwd("C:/Users/kdono/OneDrive - University of North Carolina at Chapel Hill/Documents/School/Research/Truong Research/Stats_R_Teaching/11_12_2020")
+# Works!
+getwd()
+# Print out current WD to verify all is good
+# NOTE:
+# when opening up R Studio by double clicking on a script, the WD is set to the script location automatically
+
+# Ok, how about opening a file in R (CSV for example)
+# We again need to tell R where the file is located
+data <- read.csv(file = "C:/Users/kdono/OneDrive - University of North Carolina at Chapel Hill/Documents/School/Research/Truong Research/Stats_R_Teaching/Data/AOSI_small.csv", na.strings = ".")
+
+## Relative path names
+# But this is very cumbersome.  Let's use our WD combined with relative path names to make this easier
+data <- read.csv(file="../Data/AOSI_small.csv", na.strings = ".")
+
+# Much cleaner.  R automatically fills in the WD address "C:/Users/kdono"...
+# Use of ../ means "go back" one folder in the WD
+
+### Subsetting objects
+## Vectors
+# Recall example
+x <- c(1,2,3,4)
+# Square brackets used to specify location in x
+x[1]
+
+# Prints 1st entry only
+x[c(1,3)]
+# Use vector c(1,3) to specify only 1st and 3rd entries
+y <- x[c(1,3)]
+# Can also save new subsetted vector
+x <- x[c(1,3)]
+# Or rewrite 
+
+## Matrices
+# Matrices have 2 dimensions (rows AND columns)
+# Recall 
+x <- matrix(c(1,2,3,4), nrow=2, ncol=2)
+# Again use square brackets, but now have two slots: 1) rows 2) columns
+x[1,]
+# Print 1st row, blank second slot -> all columns
+x[,1]
+# Print 1st column, blank first slot -> all rows
+x[1,1]
+# Print 1st row, 1st column
+x[1,c(1,2)]
+# Print 1st row, 1st and 2nd column
+
+## Lists
+# Lists can have many (2+) dimensions
+# Recall
+x <- list(c(1,2,3,4), c(5,6,7,8))
+
+# To specify outer layer of list, use [[ ]] instead of []
+x[[1]]
+# Prints first outer layer of list, vector c(1,2,3,4)
+x[[2]]
+
+# Now, within a given outer layer, can subset further based on layer type
+x[[1]][c(1,2)]
+# 1st outer layer is a vector, so can now use vector rules for subsetting
+x[c(1,2)]
+# To specify multiple outer layers, need to use [] instead
+
+# What if we give the outer layers names?
+x <- list("obj_1"=c(1,2,3,4), "obj_2"=c(5,6,7,8))
+x[["obj_1"]]
+# Can use names to subset as well (note use of "".  R doesn't know what obj_1 is since it does not stand alone)
+x$obj_1
+# Can also use $ instead, note "" not needed.  $ is a specically formatted shortcut so it doesn't follow the "" rule
+
+## Data frames
+# Data frames are just lists, so we can use the examples above 
+data <- read.csv(file="../Data/AOSI_small.csv", na.strings = ".")
+data[[1]]
+# Each column of a data frame is an element of the list.  We select the 1st column
+data$Identifiers
+# Can also use it's name
+
+# One benefit of data frames is that they appear like matrices.  We also use the matrix subsetting rules with data frames
+data[1,]
+# Pulls out all data frame 1st subject
+data[,1]
+# 1st vairable/column in data
+data[c(1,2,3),]
+# 1st three subjects' data
+
+# To come: advanced subsetting through tidyverse ...
---
+++ 11_12_2020/script_2.R
@@ -0,0 +1,97 @@
+### Tidyverse
+# A MUCH better way of wrangling data
+install.packages("tidyverse")
+# Make sure the tidyverse package is installed
+library(tidyverse)
+# Load tidyverse
+# tidyverse is actually a collection of a bunch of separate packages (see console)
+
+data <- read.csv(file="../Data/AOSI_small.csv", na.strings = ".")
+names(data) # quick way of viewing all variables in data frame
+
+## Select
+#  select() function used to remove variables and/or order them
+select(data, Identifiers, GROUP)
+# First argument is dataset, next arguments are variables you want to keep
+# in order you want them to appear (left to right)
+select(data, -Identifiers, -GROUP)
+# Can also specify variable you want to remove using - prefix
+select(data, GROUP, everything())
+# Can use everything() to fill in all other data variables in order they orginally appeared
+# useful when reordering the variables 
+
+## Arrange
+# arrange() function used to order rows (observations) based on variables
+arrange(data, V06.aosi.Candidate_Age)
+# Sorting by ascending age.  Note dataset is again 1st argument followed by variables
+
+arrange(data, GROUP, V06.aosi.Candidate_Age)
+# Can also do nested sorting using multiple variables (first by diagnosis, then age)
+
+arrange(data, GROUP, desc(V06.aosi.Candidate_Age))
+# Can also specify descending order for specific variable(s) using desc() function
+
+## Mutate
+# mutate() function used to create new variables
+mutate(data, 
+       v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE))
+# creating Z score for 6 month AOSI total score.  Not use of mean() to compute mean, sd() to compute standard deviation,
+# and na.rm=TRUE to remove missing values prior to calc of mean, sd.  What happens if I omit na.rm=TRUE?
+
+mutate(data, 
+       v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+       v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE))
+# Can create multiple variables, just separate by comma
+mutate(data, 
+       v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+       v06_aosi_round = round(v06_aosi_zscore, digits = 3))
+# Can also create variables based on other created variables in same mutate call.
+# Here I round my Z-scores to 3 places after creating them
+
+## Filter
+# filter() function used to remove rows of data based on variables
+filter(data, GROUP=="HR_ASD")
+# Keep only rows with GROUP="HR_ASD".  Use of == tests if "is equal to".  Remember "=" saves values to an object like <-
+filter(data, GROUP=="HR_ASD"&V06.aosi.total_score_1_18>10)
+# Keep only rows with GROUP="HR_ASD" & AOSI at 6 months > 10.  Use of & means BOTH need to be true to be in data
+filter(data, GROUP=="HR_ASD"|V06.aosi.total_score_1_18<10)
+# Use of | means "OR", only one (or more) needs to be true to be kept
+filter(data, GROUP!="HR_ASD")
+# Use of != denotes "NOT equal to", only those NOT equal to "HR_ASD" kept
+
+## Pipe: %>%
+# Notice all of these functions have the dataset as the first argument
+# Suppose we have multiple data processing steps we want to do
+data_1 <- mutate(data, 
+       v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+       v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE))
+
+filter(data_1, GROUP=="HR_ASD")
+# Very cumbersome, need to save each intermediate step as a new R object
+# Instead, let's use the pipe operator, denoted by %>%
+
+mutate(data, v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+                 v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE)) %>%
+  filter(GROUP=="HR_ASD")
+# This results in the same output, and in fact carries out the same processes.
+# How does this work?
+
+# %>% simply takes everything from the left of the symbol, and pastes it into the first
+# argument for the function call on the right.  That is, the above is the same as
+filter(mutate(data, v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+              v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE)), GROUP=="HR_ASD")
+# But MUCH CLEANER!
+# We also include it in the beginning
+data %>% 
+  mutate(v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+                v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE)) %>%
+  filter(GROUP=="HR_ASD")
+
+# and then save as a new dataset
+data_v2 <-
+  data %>% 
+  mutate(v06_aosi_zscore = (V06.aosi.total_score_1_18-mean(V06.aosi.total_score_1_18, na.rm=TRUE))/sd(V06.aosi.total_score_1_18, na.rm=TRUE),
+         v12_aosi_zscore = (V12.aosi.total_score_1_18-mean(V12.aosi.total_score_1_18, na.rm=TRUE))/sd(V12.aosi.total_score_1_18, na.rm=TRUE)) %>%
+  filter(GROUP=="HR_ASD")
+
+
